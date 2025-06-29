431b9282dde9d8f9a44dab77c9c60c84f274ed82
a.akinrinlola <a.akinrinlola@hotmail.com>
2025-06-30 01:07:15 +0200

    Tour Search hinzufuegung

---
+++ src/app/components/tour-search/tour-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './tour-search.component.html',
+  styleUrl: './tour-search.component.scss'
+})
+export class TourSearchComponent {
+
+}
