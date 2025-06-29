f0168e248b33e0e29050844c54739e942bf95ba7
a.akinrinlola <a.akinrinlola@hotmail.com>
2025-06-29 02:12:03 +0200

    TourExport.ts grober Aufbau

--- src/app/components/tour-exports/tour-exports.component.ts
+++ src/app/components/tour-exports/tour-exports.component.ts
@@ -10,6 +10,26 @@ import {CommonModule} from "@angular/common";
   styleUrl: './tour-exports.component.scss'
 })
 export class TourExportsComponent {
+  constructor(private http: HttpClient) {
+  }
 
+  exportJson(): void {
+    this.http.get<Blob>('http://localhost:8080/api/tours/export', { responseType: 'blob' as 'json' })
+      .subscribe(blob => this.downloadFile(blob, 'tours.json'));
+  }
+
+  exportCsv(): void {
+    this.http.get<Blob>('http://localhost:8080/api/tours/export/csv', { responseType: 'blob' as 'json' })
+      .subscribe(blob => this.downloadFile(blob, 'tours.csv'));
+  }
+
+  private downloadFile(data: Blob, filename: string): void {
+    const url = window.URL.createObjectURL(data);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+  }
 
 }
